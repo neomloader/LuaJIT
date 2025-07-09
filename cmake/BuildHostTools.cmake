01ecc8426f30263b4ade1ffc41ba6f97a2b37535
amogus <102964988+JustInPaper@users.noreply.github.com>
2025-07-09 14:47:06 -0600

    Update BuildHostTools.cmake

--- cmake/BuildHostTools.cmake
+++ cmake/BuildHostTools.cmake
@@ -223,6 +223,7 @@ function(luajit_build_host_tools)
 
     # buildvm
     add_executable(buildvm ${HOST_TOOL_BUILDVM_SOURCES} "${CMAKE_CURRENT_BINARY_DIR}/buildvm_arch.h")
+    get_property(LUAJIT_INCLUDE_DIR GLOBAL PROPERTY LUAJIT_INCLUDE_DIR)
     target_include_directories(buildvm PRIVATE
         ${CMAKE_CURRENT_BINARY_DIR}
         ${HOST_TOOL_BUILDVM_INCLUDES}
